ad7e87a4f6eca693af19c94aa8aa73189baadd3e
tdpearson <tylerdpearson@gmail.com>
2017-03-23 12:57:25 -0500

    add functionality to remove working files when done

--- islandoraq/tasks/tasks.py
+++ islandoraq/tasks/tasks.py
@@ -1,5 +1,7 @@
 from celery.task import task
 from subprocess import check_call, CalledProcessError
+from shutil import rmtree
+from tempfile import mkdtemp
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -16,17 +18,20 @@ def ingest_recipe(recipe_url, collection='islandora:bookCollection'):
       recipe_url: URL pointing to json formatted recipe file
       collection: Name of Islandora collection to ingest to. Default is: islandora:bookCollection  
     """
+    tmpdir = mkdtemp(prefix="recipeloader_")
     try:
         check_call(['drush',
                     '-u', '1',
                     'oubib',
                     '--recipe_uri={0}'.format(recipe_url),
                     '--parent_collection={0}'.format(collection),
-                    '--tmp_dir=/tmp',
+                    '--tmp_dir={0}'.format(tmpdir),
                     '--root=/srv/repository/drupal/'])
     except CalledProcessError as err:
         logging.error(err)
         return({"ERROR": err})
+    finally:
+        rmtree(tmpdir)
 
     return("SUCCESS")  # TODO: return islandora url for ingested book
 
