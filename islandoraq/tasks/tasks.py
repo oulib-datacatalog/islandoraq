a71c41739f96131a5babc47efebddb10cded5939
tdpearson <tylerdpearson@gmail.com>
2017-04-05 15:19:36 -0500

    yep

--- islandoraq/tasks/tasks.py
+++ islandoraq/tasks/tasks.py
@@ -9,7 +9,7 @@ from celeryconfig import ISLANDORA_DRUPAL_ROOT
 
 logging.basicConfig(level=logging.INFO)
 
-logging.info(ISLANDORA_DRUPAL_ROOT)
+logging.warn(ISLANDORA_DRUPAL_ROOT)
 
 
 @task()
@@ -32,7 +32,7 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
                         '--recipe_uri={0}'.format(recipe_url),
                         '--parent_collection={0}'.format(collection),
                         '--tmp_dir={0}'.format(tmpdir),
-                        '--root={0}'.format('/srv/test/drupal/')])
+                        '--root={0}'.format(ISLANDORA_DRUPAL_ROOT)])
         except CalledProcessError as err:
             logging.error(err)
             return({"ERROR": err})
