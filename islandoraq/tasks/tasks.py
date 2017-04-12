912b3e6857b6833c24eced37240fb2a31a174cdb
tdpearson <tylerdpearson@gmail.com>
2017-04-12 10:15:04 -0500

    additional debug logging

--- islandoraq/tasks/tasks.py
+++ islandoraq/tasks/tasks.py
@@ -10,7 +10,7 @@ import grp
 
 from celeryconfig import ISLANDORA_DRUPAL_ROOT
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 needed_paths = ["/opt/php/bin", "/opt/d7/bin"]
 
@@ -26,9 +26,12 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
       recipe_urls: List of URLs pointing to json formatted recipe files
       collection: Name of Islandora collection to ingest to. Default is: islandora:bookCollection  
     """
-    
+    logging.debug("ingest recipe args: {0}, {1}".format(recipe_urls, collection))
+    logging.debug("Environment: {0}".format(environ))
     for recipe_url in recipe_urls:
+        logging.debug("ingesting: {0}".format(recipe_url))
         tmpdir = mkdtemp(prefix="recipeloader_")
+        logging.debug("created working dir: {0}".format(tmpdir))
         chmod(tmpdir, 0o775)
         chown(tmpdir, -1, grp.getgrnam("apache").gr_gid)
         try:
@@ -47,6 +50,7 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
             return({"ERROR": "Ingest command failed"})
         finally:
             #rmtree(tmpdir)
+            logging.debug("removed working dir")
             pass
 
         return("SUCCESS")  # TODO: return islandora url for ingested book
