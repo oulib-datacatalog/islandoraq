db65306a3928aa4ba1b8c160b5e761b9d566a4fd
tdpearson <tylerdpearson@gmail.com>
2017-04-12 10:24:07 -0500

    changed debug to error to force logging on system

--- islandoraq/tasks/tasks.py
+++ islandoraq/tasks/tasks.py
@@ -10,7 +10,7 @@ import grp
 
 from celeryconfig import ISLANDORA_DRUPAL_ROOT
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 needed_paths = ["/opt/php/bin", "/opt/d7/bin"]
 
@@ -26,12 +26,12 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
       recipe_urls: List of URLs pointing to json formatted recipe files
       collection: Name of Islandora collection to ingest to. Default is: islandora:bookCollection  
     """
-    logging.debug("ingest recipe args: {0}, {1}".format(recipe_urls, collection))
-    logging.debug("Environment: {0}".format(environ))
+    logging.error("ingest recipe args: {0}, {1}".format(recipe_urls, collection)) # debug
+    logging.error("Environment: {0}".format(environ)) # debug
     for recipe_url in recipe_urls:
-        logging.debug("ingesting: {0}".format(recipe_url))
+        logging.error("ingesting: {0}".format(recipe_url)) # debug
         tmpdir = mkdtemp(prefix="recipeloader_")
-        logging.debug("created working dir: {0}".format(tmpdir))
+        logging.error("created working dir: {0}".format(tmpdir)) # debug
         chmod(tmpdir, 0o775)
         chown(tmpdir, -1, grp.getgrnam("apache").gr_gid)
         try:
@@ -50,7 +50,7 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
             return({"ERROR": "Ingest command failed"})
         finally:
             #rmtree(tmpdir)
-            logging.debug("removed working dir")
+            logging.error("removed working dir") # debug
             pass
 
         return("SUCCESS")  # TODO: return islandora url for ingested book
