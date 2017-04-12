83a7e85a56c2c89844aea66d369f125bceab3f7d
tdpearson <tylerdpearson@gmail.com>
2017-04-12 16:56:55 -0500

    include requests in setup requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='islandoraq',
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
+          'requests==2.9.1',
       ],
       include_package_data=True,
 )
