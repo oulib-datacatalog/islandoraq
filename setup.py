adc521beb24db2f2d8ef7c73609265baff3a5a7e
tdpearson <tylerdpearson@gmail.com>
2017-04-27 11:32:56 -0500

    additional testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='islandoraq',
-      version='0.2.6.45',
+      version='0.2.6.46',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
