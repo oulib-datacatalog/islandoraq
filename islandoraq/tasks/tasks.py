3f50ae9b157ee7b66611dce875d974cea468b63c
tdpearson <tylerdpearson@gmail.com>
2017-04-27 10:29:41 -0500

    check url earlier

--- islandoraq/tasks/tasks.py
+++ islandoraq/tasks/tasks.py
@@ -38,30 +38,30 @@ def ingest_recipe(recipe_urls, collection='islandora:bookCollection'):
     success = []
     for recipe_url in recipe_urls:
         logging.debug("ingesting: {0}".format(recipe_url.strip()))
-        tmpdir = mkdtemp(prefix="recipeloader_")
-        logging.debug("created working dir: {0}".format(tmpdir))
-        chmod(tmpdir, 0o775)
-        chown(tmpdir, -1, grp.getgrnam("apache").gr_gid)
-        try:
-            testresp = requests.head(recipe_url, allow_redirects=True)
-            if testresp.status_code == requests.codes.ok:
+        testresp = requests.head(recipe_url, allow_redirects=True)
+        if testresp.status_code == requests.codes.ok:
+            tmpdir = mkdtemp(prefix="recipeloader_")
+            logging.debug("created working dir: {0}".format(tmpdir))
+            chmod(tmpdir, 0o775)
+            chown(tmpdir, -1, grp.getgrnam("apache").gr_gid)
+            try:
                 drush_response = check_output(
                     ingest_template.format(recipe_url.strip(), collection, tmpdir, ISLANDORA_DRUPAL_ROOT),
                     shell=True
                 )
                 logging.debug(drush_response)
                 success.append(recipe_url)
-            else:
-                logging.error("Issue getting recipe at: {0}".format(recipe_url))
-                fail.append([recipe_url, "Server status {0}".format(testresp.status_code)])
-        except CalledProcessError as err:
-            fail.append([recipe_url, "Drush status {0}".format(err.returncode)])
-            logging.error(drush_response)
-            logging.error(err)
-            logging.error(environ)
-        finally:
-            rmtree(tmpdir)
-            logging.debug("removed working dir")
+            except CalledProcessError as err:
+                fail.append([recipe_url, "Drush status {0}".format(err.returncode)])
+                logging.error(drush_response)
+                logging.error(err)
+                logging.error(environ)
+            finally:
+                rmtree(tmpdir)
+                logging.debug("removed working dir")
+        else:
+            logging.error("Issue getting recipe at: {0}".format(recipe_url))
+            fail.append([recipe_url, "Server status {0}".format(testresp.status_code)])
             
     return ({"Successful": success, "Failures": fail})
 
