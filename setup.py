767787d805876ec0c5831947e6c2111042f48eb5
Tyler Pearson <tdpearson@users.noreply.github.com>
2018-10-30 18:25:56 -0500

    update dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='islandoraq',
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
-          'requests==2.9.1',
+          'requests==2.20.0',
           'pycurl==7.43.0',
       ],
       include_package_data=True,
