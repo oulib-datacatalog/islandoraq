781aa160fe3cf722d8753b0475d7bd0a3b4b828e
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-08-04 18:20:32 -0500

    upgrade requests

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='islandoraq',
-      version='0.2.7.2',
+      version='0.2.8',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
-          'requests==2.20.0',
+          'requests==2.24.0',
           'pycurl==7.43.0',
       ],
       include_package_data=True,
