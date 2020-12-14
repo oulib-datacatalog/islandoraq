b6761d1ec82419f52a68d2ea3ef35b1a83e7e017
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-12-14 11:30:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='islandoraq',
-      version='0.3.1',
+      version='0.3.2',
       packages= find_packages(),
       install_requires=[
           'celery',
