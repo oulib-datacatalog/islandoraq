739b3ba473dda3392c956342adf6691333c64bc3
Tyler Pearson <tdpearson@users.noreply.github.com>
2023-03-03 11:55:28 -0600

    update dependencies

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='islandoraq',
-      version='0.3.5',
+      version='0.3.6',
       packages= find_packages(),
       install_requires=[
-          'celery==5.2.2 ; python_version >= "3.7"',
+          'celery==5.2.7 ; python_version >= "3.7"',
           'celery==3.1.22 ; python_version == "2.7"',
-          'pymongo==3.12.3; python_version > "2.7"',
+          'pymongo==4.3.3; python_version >= "3.7"',
           'pymongo==3.2.1; python_version == "2.7"',
-          'requests==2.24.0',
+          'requests==2.28.2; python_version >= "3.7"',
+          'requests==2.24.0; python_version == "2.7"',
       ],
       include_package_data=True,
 )
